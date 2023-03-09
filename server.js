7229151240c93131025bbc43bd6445847a368385
Phil Williams <s222574652@deakin.edu.au>
2023-03-09 17:40:06 +1100

    save

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 //app.use(express.static(__dirname + "/public"))
 //app.use(express.urlencoded({ extended: false }));
-
-app.get("/",
-    function (res, req) {
-        res.send("hello world");
-    });
+function gettime() {
+  var datetime = new Date();
+  return datetime.toLocaleString(); // -> "2/1/2013 7:37:08 AM"
+}
+app.get("/", function (req, res) {
+  res.send("hello world");
+  console.log(gettime(), "[Server Activity]: Request Recived");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log("Server has started at port " + PORT));
