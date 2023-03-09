f3ba2a7228c1eade05f623c93b314303270f49a2
DaGoblin <s222574652@deakin.edu.au>
2023-03-09 17:29:03 +1100

    save

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(__dirname + "/public"))
+//app.use(express.urlencoded({ extended: false }));
 
+app.get("/",
+    function (res, req) {
+        res.send("hello world");
+    });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log("Server has started at port " + PORT));
